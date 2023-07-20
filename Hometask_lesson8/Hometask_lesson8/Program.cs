bf8f7517895faad84bc3b69282a1f8b2c6ade423
kamolbekb <118444680+kamolbekb@users.noreply.github.com>
2023-07-20 17:15:36 +0500

    Third lesson was tested

--- Hometask_lesson8/Hometask_lesson8/Program.cs
+++ Hometask_lesson8/Hometask_lesson8/Program.cs
@@ -2,8 +2,32 @@
 {
     internal class Program
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
+            //Console.Write("Please enter your full name: ");
+            //string fullName = Console.ReadLine();
+            //string[] array = fullName.Split(' ');
+            //string initials = "";
+            //foreach (string output in array)
+            //{
+            //    initials += output.Substring(0, 1).ToUpper() + '.';
+            //}
+            //Console.Write("Result: " + initials);
+
+            Console.Write("Please enter your sentences: ");
+            string longestWord = Console.ReadLine();
+            string[] input = longestWord.Split(" ");
+            string result = "";
+            int count = 0;
+            foreach (string str in input)
+            {
+                if (str.Length > count)
+                {
+                    result = str;
+                    count = str.Length;
+                }
+            }
+            Console.Write(result);
 
         }
     }
