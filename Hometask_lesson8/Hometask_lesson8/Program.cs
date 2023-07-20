4173f276f704b4ee490594ef6bf817c72fd3cabc
kamolbekb <118444680+kamolbekb@users.noreply.github.com>
2023-07-20 17:17:55 +0500

    4-th task was tested

--- Hometask_lesson8/Hometask_lesson8/Program.cs
+++ Hometask_lesson8/Hometask_lesson8/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
-
+            Console.Write("Please enter your sentences: ");
+            string input = Console.ReadLine();
+            int countV = 0, countC = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLetter(input[i]) && (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u'))
+                {
+                    countV++;
+                }
+                else if (char.IsLetter(input[i]) && (!(input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')))
+                {
+                    countC++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (countV > countC)
+            {
+                Console.Write($"There are {countV} vowels in the sentence");
+            }
+            else
+            {
+                Console.Write($"There are {countC} consonants in the sentence");
+            }
         }
     }
 }
